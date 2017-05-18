1a29fb22908693af7051a8a0745c075e0ed5ff99
Paco <pakokrew@gmail.com>
2017-05-18 17:25:32 +0200

    Fix merge with trustable asset

--- app/js/containers/TrustableAsset/component.js
+++ app/js/containers/TrustableAsset/component.js
@@ -1,14 +1,25 @@
 import React, { PropTypes } from 'react';
+import { StellarTools } from 'stellar-toolkit';
 
-import AssetIcon from 'js/components/ui/AssetIcon';
+import AssetIcon from 'js/containers/AssetIcon';
 import styles from './style.scss';
 
 function TrustableAsset({ wilsonAsset, trustAsset }) {
   const { code } = wilsonAsset;
+
+  // TODO: do it better
+  const balance = {
+    balance: 0,
+    wilsonAsset,
+    asset: StellarTools.AssetInstance({
+      asset_code: wilsonAsset.code,
+      asset_issuer: wilsonAsset.issuer,
+    }),
+  };
   return (
     <div className={styles.container}>
       <a className={styles.add} onClick={trustAsset}>+ Add to your wallets</a>
-      <AssetIcon knownAsset={wilsonAsset} />
+      <AssetIcon balance={balance} />
       <span className={styles.code + ' ' + styles['code-'+code]}>{code}</span>
     </div>
   );
