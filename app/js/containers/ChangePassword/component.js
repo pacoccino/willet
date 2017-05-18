08d6fa07ccd91e8138f78746726789e55fce7fab
Paco <pakokrew@gmail.com>
2017-05-18 16:17:50 +0200

    Update stellar-toolkit and fix set password

--- app/js/containers/ChangePassword/component.js
+++ app/js/containers/ChangePassword/component.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Field, propTypes } from 'redux-form';
+
 import Input from 'js/components/ui/Input';
 import Button from 'js/components/ui/OperationButton';
+import Loader from 'js/components/ui/Loader';
 
 import styles from './style.scss';
 
 function ChangePassword({ handleSubmit, pristine, submitting, submitSucceeded, submitFailed }) {
+  if(submitting) {
+    return (
+      <div className={styles.container}>
+        <Loader />
+        <p className={styles.message}>Changing password ...</p>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.container}>
       <span className={styles.title}>Change password</span>
